2d1e0c05cbe9d997421c68b000f4c28840aadda2
Promode <pramoddutta_96@yahoo.com>
2025-02-15 10:35:01 +0530

    15th Feb - P1

---
+++ src/ex_19_OOPs_Part2/hierarchical/Father.java
@@ -0,0 +1,13 @@
+package ex_19_OOPs_Part2.hierarchical;
+
+public class Father {
+
+
+    void home(){
+        System.out.println("3BHK");
+    }
+
+
+}
+
+
---
+++ src/ex_19_OOPs_Part2/singleinheritance/example1/Son2.java
@@ -0,0 +1,5 @@
+package ex_19_OOPs_Part2.singleinheritance.example1;
+
+public class Son2 extends Father{
+    void bhk4(){}
+}
